3c202abf243a4ac7f86eca47c8a8a82b6d004d10
ag502 <ag502@naver.com>
2022-05-08 21:49:42 +0900

    add: SignInForm

---
+++ src/components/SignInForm/SignInForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form class="form" @submit="onSubmitForm">
+    <div class="field">
+      <input type="text" id="login" placeholder="Login" />
+    </div>
+    <div class="field">
+      <input type="text" id="password" placeholder="Password" />
+    </div>
+    <div>
+      <button class="button" type="submit">Submit</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SignInForm",
+
+  methods: {
+    onSubmitForm(e) {
+      e.prevenDefault();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./signUpForm.scss";
+</style>
