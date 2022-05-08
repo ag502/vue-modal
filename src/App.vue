6cd1dda8e94de724f8dd0783da4d86750d76c353
ag502 <ag502@naver.com>
2022-05-08 22:03:02 +0900

    add: Modal and Form

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,7 @@
   <div id="app">
     <header>
       <h1>Modular modals in Vue from scratch</h1>
-      <p>
-        Click one of the buttons to open corresponding popup or click popup
-        backdrop to close it
-      </p>
+      <p>Click one of the buttons to open corresponding popup or click popup backdrop to close it</p>
     </header>
 
     <main>
@@ -16,12 +13,28 @@
         <button>Sign up</button>
       </div>
     </main>
+
+    <ModalContainer v-show="false">
+      <SignInForm />
+    </ModalContainer>
+
+    <ModalContainer v-show="false">
+      <SignUpForm />
+    </ModalContainer>
   </div>
 </template>
 
 <script>
+import { ModalContainer, SignInForm, SignUpForm } from "./components";
+
 export default {
   name: "App",
+
+  components: {
+    ModalContainer,
+    SignInForm,
+    SignUpForm,
+  },
 };
 </script>
 
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as ModalContainer } from "./ModalContainer/ModalContainer.vue";
+export { default as SignInForm } from "./SignInForm/SignInForm.vue";
+export { default as SignUpForm } from "./SignUpForm/SignUpForm.vue";
