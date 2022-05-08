7d0d9c2d14fcacecf8475a349bd0823e2a58509f
ag502 <ag502@naver.com>
2022-05-08 21:56:58 +0900

    add: SignUpForm

---
+++ src/components/SignUpForm/SignUpForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form class="form" @submit="onSubmitForm">
+    <div class="field">
+      <input type="text" placholder="Login" id="login" />
+    </div>
+    <div class="field">
+      <input type="email" placeholder="Email" id="email" />
+    </div>
+    <div class="field">
+      <button class="button" type="submit">Submit</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SignUpForm",
+
+  methods: {
+    onSubmitForm(e) {
+      e.preventDefault();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./signUpForm.scss";
+</style>
